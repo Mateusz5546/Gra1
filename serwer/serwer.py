019a9050db8e074a5c2912fe9b0766547672e79b
Mateusz5546 <106268498+Mateusz5546@users.noreply.github.com>
2024-05-16 10:45:49 +0200

    Add files via upload

---
+++ serwer/serwer.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import main
+import Function
+import os
+# try:
+#     os.unlink("../gra.db")
+# except:
+#     pass
+
+
+
+
+app = FastAPI()
+
+main.zainicjuj()
+
+@app.get("/countries")
+def countries():
+    return Function.name_country()
+@app.get("/country/{kraj_id}")
+def country(kraj_id):
+    return Function.name_country_resorces(kraj_id)
+@app.get("/build_structure/{kraj_id}/{budynek_id}")
+def build_structure(kraj_id:int,budynek_id:int):
+    return Function.build_structure(budynek_id,kraj_id)
+@app.get("/recruit_army")
+def recruit_army(kraj_id:int,liczba_jednostek:int):
+    return Function.recruit_army(kraj_id,liczba_jednostek)
